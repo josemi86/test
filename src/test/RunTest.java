08c15bab6730629e202782e96fb282dced87b4fb
josemi86 <jmcardenas86@gmail.com>
2020-11-12 13:21:03 -0600

    review adjustments

--- src/test/RunTest.java
+++ src/test/RunTest.java
@@ -3,42 +3,31 @@ package test;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Optional;
 
 public class RunTest {
 
     private static List<Person> people = loadPeople();
 
     public static void main(String[] args) {
-        if (isCelebrity("Brian"))
-            System.out.println("Is celebrity!!!");
+        Optional<Person> celebrity = getCelebrity();
+        if (celebrity.isPresent()) { 
+            System.out.println("Celebrity is " + celebrity.get()); 
+        } 
+        else { 
+            System.out.println("No celebrity"); 
+        } 
     }
-
-    private static boolean isCelebrity(String name) {
-        boolean response = false;
-        for (Person person : people) {
-            if (person.getName().equals(name) && person.getKnownPeople().size() == 0 && isKnownByAll(person)) {
-                response = true;
-            }
-        }
-        return response;
+    
+    private static Optional<Person> getCelebrity() {
+        return people.stream().filter(person->person.getKnownPeople().size() == 0 && isKnownByAll(person)).findFirst();
     }
 
     private static boolean isKnownByAll(Person person) {
         boolean response = true;
-        for (Person otherPerson : people) {
-            if (!otherPerson.getName().equals(person.getName()) && !isKnownByPerson(person, otherPerson)) {
-                response = false;
-            }
-        }
-        return response;
-    }
-
-    private static boolean isKnownByPerson(Person person, Person otherPerson) {
-        boolean response = false;
-        for (Person knownPerson : otherPerson.getKnownPeople()) {
-            if (knownPerson.getName().equals(person.getName()))
-                response = true;
-        }
+        if(people.stream().anyMatch(otherPerson->!otherPerson.getName().equals(person.getName()) 
+                    && !otherPerson.getKnownPeople().contains(person)))
+            response = false;
         return response;
     }
 
@@ -47,7 +36,7 @@ public class RunTest {
         Person celebrity = new Person("Brian", new ArrayList<Person>());
         Person person1 = new Person("John", Arrays.asList(celebrity));
         Person person2 = new Person("Jack", Arrays.asList(celebrity, person1));
-        Person person3 = new Person("James", Arrays.asList(celebrity, person1, person2));
+        Person person3 = new Person("James", Arrays.asList(person1, person2));
         people.add(person1);
         people.add(person2);
         people.add(person3);
